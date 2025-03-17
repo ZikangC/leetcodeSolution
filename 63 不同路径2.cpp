b9eadfac0bf4de6cf82f2b71148ba031500c5183
Zikang Chen <136550709+ZikangC@users.noreply.github.com>
2025-03-17 17:02:01 +0800

    Create 63 不同路径2.cpp

---
+++ 63 不同路径2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int row = obstacleGrid.size();
+        int col = obstacleGrid[0].size();
+        vector<vector<int>> dp(row,vector<int>(col,0));
+        for(int i = 0; i < col; i++){
+            if(obstacleGrid[0][i] == 1){
+                break;
+            }
+            dp[0][i] = 1;
+        }for(int i = 0; i < row; i++){
+            if(obstacleGrid[i][0] == 1){
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(obstacleGrid[i][j] == 0){
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[row-1][col-1];
+    }
+};
