1fe7d0693ac17e5ac45a264b438b964e4962929c
Zikang Chen <136550709+ZikangC@users.noreply.github.com>
2025-03-18 11:36:11 +0800

    Create 96 不同的二叉搜索树.cpp

---
+++ 96 不同的二叉搜索树.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        if(n == 1){
+            return 1;
+        }
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i < n+1; i++){
+            for(int j = 0; j < i; j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+};
