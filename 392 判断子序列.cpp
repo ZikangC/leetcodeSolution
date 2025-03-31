dede6486e638e307912a0f7cc2861ff12f3ddb61
Zikang Chen <136550709+ZikangC@users.noreply.github.com>
2025-03-31 10:54:37 +0800

    Update 392 判断子序列.cpp

--- 392 判断子序列.cpp
+++ 392 判断子序列.cpp
@@ -17,3 +17,26 @@ public:
         return false;
     }
 };
+
+#使用dp的方法
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int slen = s.length();
+        int tlen = t.length();
+        vector<vector<bool>> dp(slen+1,vector<bool>(tlen+1,false));
+        for(int i = 0; i <= tlen; i++){
+            dp[0][i] = true;
+        }
+        for(int i = 1; i <= slen; i++){
+            for(int j = 1; j <= tlen; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        return dp[slen][tlen];
+    }
+};
