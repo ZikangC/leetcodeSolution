f51ba4f9a6bbfc88938a71fb613b5795030fb901
Zikang Chen <136550709+ZikangC@users.noreply.github.com>
2025-08-13 14:50:58 +0800

    Create 48 旋转矩阵.cpp

---
+++ 48 旋转矩阵.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int len = matrix.size();
+        for(int i = 0; i < len; i++){
+            for(int j = i+1; j < len; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len/2; j++){
+                swap(matrix[i][j],matrix[i][len - j - 1]);
+            }
+        }
+        return;
+    }
+};
